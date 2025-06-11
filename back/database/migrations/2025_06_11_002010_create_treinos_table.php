b01a4c3688bb115ebedb085f86597f1e0317359f
FranciscoDetoni <franciscofdetoni@gmail.com>
2025-06-10 21:27:05 -0300

    Criação da migration de treinos
    Tabela de treinos com id, exercicio, series, repeticoes e carga!

---
+++ back/database/migrations/2025_06_11_002010_create_treinos_table.php
@@ -0,0 +1,31 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('treinos', function (Blueprint $table) {
+             $table->id(); 
+            $table->string('exercicio');
+            $table->integer('series');
+            $table->string('repeticoes');
+            $table->decimal('carga', 5, 2)->nullable(); 
+            $table->timestamps(); 
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('treinos');
+    }
+};
